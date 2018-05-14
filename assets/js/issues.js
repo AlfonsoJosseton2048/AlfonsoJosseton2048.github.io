19c93ac59ee4be0106fd479a50dfbda487f507b2
Alfonso López Ruiz <alr00048@red.ujaen.es>
2018-05-14 23:28:29 +0200

    Se pueden subir imágenes con los issues

--- assets/js/issues.js
+++ assets/js/issues.js
@@ -5,13 +5,14 @@ $(document).ready(function() {
     authDomain: "uja-2048-52718.firebaseapp.com",
     databaseURL: "https://uja-2048-52718.firebaseio.com",
     projectId: "uja-2048-52718",
-    storageBucket: "",
+    storageBucket: "uja-2048-52718.appspot.com",
     messagingSenderId: "770426857642"
   };
   firebase.initializeApp(config);
 
   // Our database
 	var db = firebase.database();
+  var storage = firebase.storage();
 
   // Listeners
   db.ref().child('issues').on('child_added', function(snapshot) {
@@ -28,27 +29,35 @@ $(document).ready(function() {
 
     var title = $('#titleInsertIssue').val();
     var description = $('#descriptionInsertIssue').val();
+    var file = document.getElementById('imageInsertIssue').files[0];
+
     // Are the input values valid?
     if (title && description) {
-      // Reference to the issues path
-      var issues = db.ref('issues');
-      var data = {
-        title: title,
-      	description: description,
-        date: Date.now()
-      }
-      issues.push(data);
+      // Do we need to upload an image? If anyone has been chosen
+      uploadIssue(title, description, file);
     } else {
-      $('#errorInsertUser').text('The title and the description of the issue cannot be empty.');
+      $('#errorInsertIssue').text('The title and the description of the issue cannot be empty.');
       $('.modal-dialog').effect('shake', {times: 2}, 600);
     }
 	});
 
+  // Click an item from the dropdown menu
   $(document).on('click', '.dropdown-item', function() {
+    // Select the new type of sort
     $('.dropdown-item').removeClass('active');
     $(this).addClass('active');
-    console.log($(this).text());
+
     sortList($(this).text());
+  });
+
+  $('#imageInsertIssue').on('change', function() {
+    var file = $(this).val();
+
+    if (file) {
+      var getName = file.split('\\');
+      file = getName[getName.length - 1];
+      $('#imageInsertIssueLabel').text(file);
+    }
   });
 
   // Exit button or Cancel were pressed, so inputs will be cleaned as well as the error message
@@ -67,12 +76,44 @@ $(document).ready(function() {
   function addIssue(key, title, description, dateTimestamp) {
     var date = new Date(dateTimestamp);
     var dateString = 'Date: ' + date.toGMTString();
-    $('#issueList').append('<li id="' + key + '" class="list-group-item"><a href=""><h6 class="m-0 p-0"><i class="mr-2 fas fa-exclamation-circle"></i>' + title + '</h6></a><p class="small text-muted m-0 p-0 pt-2">' + dateString + '</p></li>');
+    $('#issueList').append('<li id="' + key + '" class="list-group-item ml-0">' +
+                            '<button class="btn btn-link m-0 p-0" data-toggle="modal" data-target="#issueInfoModal">' +
+                              '<h6 class="m-0 p-0"><i class="mr-2 fas fa-exclamation-circle"></i>' + title + '</h6>' +
+                            '</button>' +
+                            '<p class="small text-muted m-0 p-0 pt-2 pl-4 ml-1">' + dateString + '</p>' +
+                           '</li>');
   }
 
-  // Save the identifier of the issue to show it in the new page
+  // Modify the modal information when an issue is clicked
   $(document).on('click', '.list-group-item', function() {
-    console.log('Hola');
+    // Identifier of the issue we're looking for
+    var issueId = $(this).attr('id');
+
+    db.ref().child('issues/' + issueId).once('value', function(snapshot) {
+      if (snapshot.val()) {
+        // Show body and hide error message
+        $('#bodyIssueInfo').css('display', 'block');
+        $('#errorIssueInfo').css('display', 'none');
+
+        $('#issueInfoIdentifier').text('Issue ' + issueId.replace('-', ''));
+  			$('#issueInfoTitle').text(snapshot.val().title);
+        $('#issueInfoDescription').text(snapshot.val().description);
+
+        // Does this issue have an image?
+        $('#imageBlock').css('display', 'none');  // By default the block with image will be hidden
+
+        var issuesRef = storage.ref().child('issues/' + issueId + '.png');
+        issuesRef.getDownloadURL().then(function(url) {
+          $('#imageBlock').css('display', 'block');
+          $('#issueInfoImage').attr('src', url);
+        })
+      } else {
+        // Hide the body and show the error message
+        $('#bodyIssueInfo').css('display', 'none');
+        $('#errorIssueInfo').css('display', 'block');
+      }
+    }).catch(function(error) {
+    });
   });
 
   // Sort list of issues
@@ -89,13 +130,12 @@ $(document).ready(function() {
         shouldSwitch = false;
 
         // Should we switch the rows?
-        console.log(listElements[i].getElementsByTagName('p')[0].innerHTML);
         if ((type == 'Newest') &&
-            (listElements[i].getElementsByTagName('p')[0].innerHTML > listElements[i + 1].getElementsByTagName('p')[0].innerHTML)) {
+            (listElements[i].getElementsByTagName('p')[0].innerHTML < listElements[i + 1].getElementsByTagName('p')[0].innerHTML)) {
           shouldSwitch = true;
           break;
         } else if ((type == 'Oldest') &&
-            (listElements[i].getElementsByTagName('p')[0].innerHTML < listElements[i + 1].getElementsByTagName('p')[0].innerHTML)) {
+            (listElements[i].getElementsByTagName('p')[0].innerHTML > listElements[i + 1].getElementsByTagName('p')[0].innerHTML)) {
           shouldSwitch = true;
           break;
         }
@@ -106,5 +146,32 @@ $(document).ready(function() {
         switching = true;
       }
     }
+  }
+
+  function uploadIssue(title, description, file) {
+    // We need a new key, for the image name and the identifier of the issue in the realtime database
+    var key = db.ref('issues').push().key;
+
+    if (file) {
+      var ref = storage.ref().child('issues/' + key + '.png');
+
+      try {
+        ref.put(file).then(function() {
+          uploadIssueDatabase(key, title, description);
+        });
+      } catch (error) {
+        $('#errorInsertIssue').text('An error occurred while uploading the file, so the issue couldn\'t be uploaded either.');
+      }
+    } else {
+      uploadIssueDatabase(key, title, description);
+    }
+  }
+
+  function uploadIssueDatabase(key, title, description) {
+    db.ref('issues/' + key).set({
+      title: title,
+      description: description,
+      date: Date.now()
+    });
   }
 });
